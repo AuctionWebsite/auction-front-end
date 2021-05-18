56e7747e61030d8215f4ed377405d7c5aea39daa
SFN <sfndevelopment@gmail.com>
2021-05-18 13:03:57 -0400

    Create React App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  document.title="Bid my Talent!";
+  return (
+    <div className="App">
+      <h1>Bid my Talent!</h1>
+    </div>
+  );
+}
+
+export default App;
